1977696d7913f4e28164ef609a95fdb5eb728a01
PYC <16897921+PYC12345678@user.noreply.gitee.com>
2026-07-12 14:46:23 +0800

    人事系统2

--- src/main/java/com/employeesys/EmployeeSysApplication.java
+++ src/main/java/com/employeesys/EmployeeSysApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeSysApplication {
-    111
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeSysApplication.class, args);
